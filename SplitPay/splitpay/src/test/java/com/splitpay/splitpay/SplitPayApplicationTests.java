eaffa06eb54d1f278be41b0a7b3b1f420577c904
Spyros Gavriil <spyrosgavriil01@gmail.com>
2025-06-14 14:24:27 +0300

    Initial commit

---
+++ SplitPay/splitpay/src/test/java/com/splitpay/splitpay/SplitPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitpay.splitpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
